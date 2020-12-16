803c76b04e866a62096628d71af0ae6445617207
mmekou <meka.azaeva@gmail.com>
2020-12-16 20:20:44 +0600

    game

--- newgame_bot.py
+++ newgame_bot.py
@@ -9,19 +9,22 @@ class GuessGame:
 TOKEN = '1255638994:AAG3mUlBd8lN1MsGsg6TOwvUAQUhdBnlSHU'
 bot = telebot.TeleBot(TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     sti = open('./sticker.webp','rb')
     bot.send_sticker(message.chat.id, sti)
     chat_id = message.chat.id
-    bot.send_message(chat_id,'Welcome my KIng or my Queen', reply_markup = inline_keyboard)
+    bot.send_message(chat_id,'Welcome my Dear Guest!', reply_markup=inline_keyboard)
     
 entry = {}
 inline_keyboard = types.InlineKeyboardMarkup(row_width=2)
-btn1 = types.InlineKeyboardButton('Ранг 1-100',callback_data = 'Ранг 1-100')
-btn2 = types.InlineKeyboardButton('Ранг 1-1000',callback_data = 'Ранг 1-1000')
-btn3 = types.InlineKeyboardButton('Ранг 1-10000',callback_data = 'Ранг 1-10000')
-inline_keyboard.add(btn1,btn2,btn3)
+btn1 = types.InlineKeyboardButton('Ранг 1-100', callback_data='Ранг 1-100')
+btn2 = types.InlineKeyboardButton('Ранг 1-1000', callback_data='Ранг 1-1000')
+btn3 = types.InlineKeyboardButton('Ранг 1-10000', callback_data='Ранг 1-10000')
+inline_keyboard.add(btn1, btn2, btn3)
+
+
 @bot.callback_query_handler(func=lambda c:True)
 def inline_(c):
     if c.data == 'Ранг 1-100':
@@ -68,18 +71,18 @@ def random_number(message, attempt):
             if n < number:
                 sti = open('./sticker2.webp','rb')
                 bot.send_sticker(message.chat.id, sti)
-                msg = bot.send_message(message.chat.id, 'Воу воу по легче, дай немного газу. Докинь сверу.')
+                msg = bot.send_message(message.chat.id, 'Эу ты что? дай немного газу. Число больше.')
                 bot.register_next_step_handler(msg, lambda message: random_number(message, attempt))
 
             elif n > number:
                 sti = open('./sticker3.webp','rb')
                 bot.send_sticker(message.chat.id, sti)
-                msg = bot.send_message(message.chat.id, 'Эу ты что? чуть по меньше давай. свои же)')
+                msg = bot.send_message(message.chat.id, 'Воу воу, полегче чуть поменьше число давай)')
                 bot.register_next_step_handler(msg, lambda message: random_number(message, attempt))
             else:
                 sti = open('./sticker4.webp','rb')
                 bot.send_sticker(message.chat.id, sti)
-                bot.send_message(message.chat.id, 'Поздравляем вас, с победой. Сегодня вы достойны аплодисментов!!!'.format(attempt - 1))
+                bot.send_message(message.chat.id, 'Поздравляем вас, с победой. Сегодня вы достойны апплодисментов!!!'.format(attempt - 1))
         else:
             sti = open ('./sticker7.webp', 'rb')
             bot.send_sticker(message.chat.id, sti)
